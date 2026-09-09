3ae644bc66ffa644c21baabc5c191d42fd0875c6
saharkhodaverdian <pondering.khodaverdian@gmail.com>
2026-09-09 12:50:42 +0330

    version2

--- main.js
+++ main.js
@@ -42,3 +42,48 @@ const createTween = () => {
 createTween();
 
 window.addEventListener("resize", createTween);
+
+
+/* projects slider */
+
+const swiper = new Swiper(".my-slider", {
+  slidesPerView: 4,
+  spaceBetween: 55,
+
+  loop: true,
+
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+
+  speed: 1000,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 55,
+    },
+  },
+});
